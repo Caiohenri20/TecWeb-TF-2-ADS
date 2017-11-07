1ee7cb9ab0dbbb47dce3fc454cd1aa676fc3b096
IsabellaAnze <30813710+IsabellaAnze@users.noreply.github.com>
2017-11-07 11:41:07 -0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -33,3 +33,30 @@ def formulario_cadastro(request):
     
 def pagina_aluno(request):
     return render(request, "pagina_aluno.html")
+
+def cancelamento de matrícula(request):
+    return render(request, "cancelamentodematricula.html")
+
+def exercícios e trabalhos aluno(request):
+    return render(request, "exerciciosetrabalhos_aluno.html")
+
+def horário(request):
+    return render(request, "horario.html")
+
+def contato professores(request):
+    return render(request, "contatoprofessores.html")
+
+def formulario_cancelamento(request):
+    return render(request, "formulario_cancelamento.html")
+
+def formulario_enviarmsg(request):
+    return render(request, "formulario_enviarmsg.html")
+
+def menu aluno(request):
+    return render(request, "menualuno.html")
+
+def notas(request):
+    return render(request, "notas.html")
+
+def resumo das entregas pendentes(request):
+    return render(request, "resumodasentregaspendentes.html")
